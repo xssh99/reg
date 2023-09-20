53078c9ac019e97ae3139722822653f2e9210ad6
xssh99 <145438188+xssh99@users.noreply.github.com>
2023-09-21 02:35:22 +0800

    Update jabut.sh

--- jabut.sh
+++ jabut.sh
@@ -49,12 +49,10 @@ read -p "Expired (days): " EXPIRED
 done
 until [[ $iplim =~ ^[0-9]+$ ]]; do
 read -p "Limit User (IP): " iplim
-echo > /etc/cron.d/kills
-                echo "# $user" >>/etc/cron.d/kills
-                echo "*/1 * * * *  root /usr/bin/kills $iplim" >>/etc/cron.d/kills
-                echo -e ""
-                service cron restart >/dev/null 2>&1
-                service cron reload >/dev/null 2>&1
+echo ">/etc/cron.d/kills
+echo "# $user" >/etc/cron.d/kills
+echo "*/120 * * * * root /usr/bin/kilss $iplim" >> /etc/cron.d/kills
+fi
 done
 IP=$(curl -sS ifconfig.me)
 CITY=$(cat /etc/xray/city)
